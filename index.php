7fb7973b5aec9e6aa9d04056712af20c9be330d1
danieljonas9870-cpu <danieljonas9870@gmail.com>
2025-11-04 11:03:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,40 @@
+<?php 
+
+$products = [
+    ['id'=>101,'title'=>'Fiktivní bomba (replika)','desc'=>'jen rekvizita, nefunkční','seller'=>55,'visible'=>true],
+    ['id'=>102,'title'=>'Kobereček','desc'=>'hezký kobereček','seller'=>66,'visible'=>true],
+];
+
+$keywords = ['bomb','bomba','výbušnina','explosive'];
+
+$tickets = [];
+$emails = [];
+
+foreach ($products as &$p) {
+    $hay = mb_strtolower($p['title'] . ' ' . $p['desc']);
+    $found = false;
+    foreach ($keywords as $kw) {
+        if (mb_stripos($hay, $kw) !== false) { $found = true; break; }
+    }
+
+    if ($found) {
+
+        $p['visible'] = false;
+
+        $tickets[] = ['ticket_id' => 'T' . time() . $p['id'], 'product_id' => $p['id']];
+
+        $emails[] = ['to'=>'seller' . $p['seller'] . '@example.cz', 'subject'=>"Karanténa produktu {$p['id']}"];
+    }
+}
+unset($p);
+
+print_r($products);
+print_r($tickets);
+print_r($emails);
+
+
+?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
